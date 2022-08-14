1839c988fac0aebfa76aaa6e542cad6b93458464
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-14 13:40:23 -0300

    add library requests | add url api name

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 
 def homeView(request):
+    url = 'http://api.openweathermap.org/data/2.5/forecast?d=524901&appid=135b2cbe9493a774efcf3e7177bcd6bd'
+    APPID = '135b2cbe9493a774efcf3e7177bcd6bd'    
+    
     context = {}
     return render(request, 'weatherapp/home.html', context)
